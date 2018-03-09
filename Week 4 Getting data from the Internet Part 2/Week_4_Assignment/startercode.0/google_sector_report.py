028af8245dd5bcdc63787c4ed343d2f4fa7a078a
Vadym Serpak <v.serpak@gmail.com>
2018-03-09 23:33:49 +0200

    Week 4 Assignment with data in files

---
+++ Week 4 Getting data from the Internet Part 2/Week_4_Assignment/startercode.0/google_sector_report.py
@@ -0,0 +1,20 @@
+"""
+Modify the following google_sector_report so that it returns a json 
+dump that contains the following information about each sector:
+1. The sector name
+2. The percentage change in sector value
+3. The biggest gainer and the percentage change in the biggest gainer
+4. The biggest loser and the percentage change in the biggest loser
+
+The structure of the json is given in the assignment description on EdX.
+
+Note:
+To read files, use:
+
+with open('filename') as f:
+    lines = f.readlines()
+"""
+def google_sector_report():
+    
+    
+    return json_string
